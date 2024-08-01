2beec05ba0006460cc5e2aff9d9202b9239a022a
pekuria <pekuria@hotmail.com>
2024-08-01 03:43:57 +0100

    bumped composer packages

--- resources/views/livewire/chirps/create.blade.php
+++ resources/views/livewire/chirps/create.blade.php
@@ -6,7 +6,7 @@ state(['message' => '']);
 
 rules(['message' => 'required|string|max:255']);
 
-$store = function() {
+$store = function () {
     $validated = $this->validate();
 
     auth()->user()->chirps()->create($validated);
@@ -19,23 +19,16 @@ $store = function() {
 ?>
 
 <div>
-<form wire:submit="store"> 
+    <form wire:submit="store">
 
-<textarea
-
-    wire:model="message"
-
-    placeholder="{{ __('What\'s on your mind?') }}"
-
-    class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-
-></textarea>
+        <textarea wire:model="message" placeholder="{{ __('What\'s on your mind?') }}"
+            class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"></textarea>
 
 
 
-<x-input-error :messages="$errors->get('message')" class="mt-2" />
+        <x-input-error :messages="$errors->get('message')" class="mt-2" />
 
-<x-primary-button class="mt-4">{{ __('Chirp') }}</x-primary-button>
+        <x-primary-button class="mt-4">{{ __('Chirp') }}</x-primary-button>
 
-</form>  
+    </form>
 </div>
